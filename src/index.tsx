d25c629afaf3e7ddf0f126b92a88645fd3682b64
Fabian Siffert <siffert@sipgate.de>
2022-05-13 09:01:19 +0200

    :sparkles: start the redesign of the website

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,10 @@ import {
   Route,
 } from "react-router-dom";
 
-import App from './App';
-import Biography from './routes/biography/biography';
-import Contact from './routes/contact/contact';
-import Contacts from './routes/contacts/contacts';
+import Home from './views/Home/Home';
+import Biography from './views/Biography/Biography';
+import Contact from './views/Contact/Contact';
+import Contacts from './views/Contacts/contacts';
 
 const container = document.getElementById('root');
 // value! -> TS checks that value is never null
@@ -19,7 +19,7 @@ const root = ReactDOMClient.createRoot(container!);
 root.render(<React.StrictMode>
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} >
+      <Route path="/" element={<Home />} >
         <Route path="biography" element={<Biography />} />
         <Route path="contact" element={<Contact />} >
         <Route
--- src/routes/biography/biography.tsx
+++
@@ -1,7 +0,0 @@
-export default function Biography() {
-    return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Biography</h2>
-      </main>
-    );
-  }
