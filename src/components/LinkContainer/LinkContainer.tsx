493981eb183b20b6d4ee7a04415973d5ae0fe9a1
Fabian Siffert <siffert@sipgate.de>
2022-05-13 11:08:28 +0200

    :construction: stash push

--- src/components/LinkContainer/LinkContainer.tsx
+++ src/components/LinkContainer/LinkContainer.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import language from '../../resources/language/language';
-import styles from './LinkContainer.module.scss';
+import React from "react";
+import language from "../../resources/language/language";
+import styles from "./LinkContainer.module.scss";
 
 
+//TODO: styles.header:dark -> CSS Subclass
 function LinkContainer(props: any) {
-    return (
-        <div className={styles.LinkContainer}>
-
-            <a
-                className={styles.link}
-                href={props.link}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-            <header className={styles.headerLight}>
-                <img src={props.imageSource} className={styles.logo} alt={language.LOGO} />
-            </header>
-            </a>
-        </div>
-    );
+	return (
+		<div className={styles.LinkContainer}>
+			<header className={styles.header}>
+				<a
+					className={styles.link}
+					href={props.link}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={props.imageSource}
+						className={styles.logo}
+						alt={language.LOGO}
+					/>
+				</a>
+			</header>
+		</div>
+	);
 }
 export default LinkContainer;
