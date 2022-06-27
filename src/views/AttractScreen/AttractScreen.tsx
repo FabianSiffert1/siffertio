3ffed6723dee911e592f52ee51e876895831a393
Fabian Siffert <siffert@sipgate.de>
2022-06-27 10:28:04 +0200

    :lipstick: add tool icon and change attractScreen title capitilization

--- src/views/AttractScreen/AttractScreen.tsx
+++ src/views/AttractScreen/AttractScreen.tsx
@@ -7,7 +7,7 @@ export default function AttractScreen() {
             <NavLink to={"/Biography"} key={"Biography"}>
             <div className={styles.textContent}>
                 <div className={styles.container}>
-                    <div className={styles.glitch} data-text="Siffert.io">
+                    <div className={styles.glitch} data-text="siffert.io">
                         <div className={styles.glow}>siffert.io</div>
                         <p className={styles.subtitle}>Work In Progress</p>
                     </div>
