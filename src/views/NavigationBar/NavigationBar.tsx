26b24163644439dd618d764f8e974eae3af300f5
Fabian Siffert <siffert@sipgate.de>
2022-06-27 11:01:15 +0200

    :recycle: refactor minor things and add toolSmall and toolThick svgs

--- src/views/NavigationBar/NavigationBar.tsx
+++ src/views/NavigationBar/NavigationBar.tsx
@@ -5,6 +5,7 @@ import ImageRouter from "../../components/ImageRouter/ImageRouter";
 
 import Biography from "../../resources/images/buttons/home2.svg";
 import Contact from "../../resources/images/buttons/mailClosed.svg";
+import Tool from "../../resources/images/buttons/toolSmall.svg";
 import ActiveContact from "../../resources/images/buttons/mailOpen.svg";
 import DarkMode from "../../resources/images/buttons/darkMode.svg";
 
@@ -16,6 +17,7 @@ function NavigationBar(props: any) {
         <div className={styles.NavigationItems}>
             <ImageRouter url={'/biography'} navLinkKey={'Biography'} imagePath={Biography} showTitle={true}/>
             <ImageRouter url={'/contact'} navLinkKey={'Contact'} imagePath={Contact} activeImagePath={ActiveContact} showTitle={true}/>
+            <ImageRouter url={'/contact'} navLinkKey={'Tools'} imagePath={Tool} showTitle={true}/>
             <button className={styles.settingsButton}>
                 <img className={styles.settingsButtonImage} src={DarkMode} alt="DarkModeToggle"
                      onClick={toggleDarkMode}/>
