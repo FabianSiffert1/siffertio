04a3b8fa1a14fac1944be6d59bf0f4cc7fd8908f
Fabian Siffert <siffert@sipgate.de>
2022-06-28 18:32:57 +0200

    :construction: redesign navBarLinks to Css Text, change Header Design

--- src/views/NavigationBar/NavigationBar.tsx
+++ src/views/NavigationBar/NavigationBar.tsx
@@ -1,31 +1,15 @@
 import React from "react";
 import styles from "./NavigationBar.module.scss";
 
-import ImageRouter from "../../components/ImageRouter/ImageRouter";
-
 import Biography from "../../resources/images/buttons/home2.svg";
 import Contact from "../../resources/images/buttons/mailClosed.svg";
-import Tool from "../../resources/images/buttons/toolSmall.svg";
-import ActiveContact from "../../resources/images/buttons/mailOpen.svg";
-import DarkMode from "../../resources/images/buttons/darkMode.svg";
 import {NavLink} from "react-router-dom";
 
 function NavigationBar(props: any) {
-    const toggleDarkMode = () => {
-        alert("Toggling Dark/Lightmode");
-    }
     return <div className={styles.NavigationBar}>
         <div className={styles.NavigationItems}>
-            <ImageRouter route={'/biography'} routeKey={'Biography'} imagePath={Biography} showRouteKeyAsTooltip={true}/>
-            <div className={styles.Temp}>
-                <div className={styles.animateCharacter}> <NavLink to={'/biography'} key={'Biography'}>Biography</NavLink></div>
-            </div>
-            <ImageRouter route={'/contact'} routeKey={'Projects'} imagePath={Tool} showRouteKeyAsTooltip={true}/>
-            <ImageRouter route={'/contact'} routeKey={'Contact'} imagePath={Contact} activeImagePath={ActiveContact} showRouteKeyAsTooltip={true}/>
-            <button className={styles.settingsButton}>
-                <img className={styles.settingsButtonImage} src={DarkMode} alt="DarkModeToggle"
-                     onClick={toggleDarkMode}/>
-            </button>
+                <div className={styles.activeLink}> <NavLink to={'/biography'} key={'Biography'}>Biography</NavLink></div>
+            <div className={styles.inactiveLink}> <NavLink to={'/contact'} key={'Contact'}>Contact</NavLink></div>
         </div>
     </div>
 }
