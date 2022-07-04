2484a43e23041f7d545e245c22fec17d952ce7ec
Fabian Siffert <siffert@sipgate.de>
2022-07-04 09:04:47 +0200

    :recycle: WIP: start refactoring header and fadeInText to be even more flexible

--- src/components/FadeInText/FadeInText.tsx
+++ src/components/FadeInText/FadeInText.tsx
@@ -3,10 +3,15 @@ import React from "react";
 
 export interface FadeInTextProps {
     textToDisplay?: string,
-    blockColor?: string,
-    blockHeight?: string,
-    blockWidth?: string,
+    mainBlockColor?: string,
+    mainBlockHeight?: string,
+    mainBlockWidth?: string,
+    secondaryBlockColor?: string,
+    secondaryBlockHeight?:string,
+    secondaryBlockWidth?: string,
     fadeInTime?: string,
+    mainFontSize?: string,
+    secondaryFontSize?: string,
     children?: React.ReactNode,
 }
 
@@ -16,15 +21,21 @@ export default function FadeInText(props: FadeInTextProps) {
             <div className={styles.FadeInAnimationContainer}>
                 <div className={styles.coloredBlock}
                      style={{
-                         backgroundColor: props.blockColor,
-                         height: props.blockHeight,
-                         width: props.blockWidth
+                         backgroundColor: props.mainBlockColor,
+                         height: props.mainBlockHeight,
+                         width: props.mainBlockWidth,
+                         fontSize: props.mainFontSize,
                      }}></div>
                 <div className={styles.fadeInText}>
                     {props.textToDisplay && props.textToDisplay}
                 </div>
             </div>
-            <div className={styles.FadeInPlainTextContainer}>
+            <div className={styles.FadeInPlainTextContainer} style={{
+                backgroundColor: props.secondaryBlockColor,
+                height: props.secondaryBlockHeight,
+                width: props.secondaryBlockWidth,
+                fontSize: props.mainFontSize,
+            }}>
                 {props.children}
             </div>
         </div>
