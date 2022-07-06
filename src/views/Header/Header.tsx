5d4fb7d995d39c86e72a1e25ecbecca562976032
Fabian Siffert <siffert@sipgate.de>
2022-07-06 18:13:04 +0200

    :iphone: change button position

--- src/views/Header/Header.tsx
+++ src/views/Header/Header.tsx
@@ -3,23 +3,26 @@ import styles from './Header.module.scss';
 import {NavLink} from "react-router-dom";
 import FadeInText from "../../components/FadeInText/FadeInText";
 
-interface headerProps{
+interface headerProps {
     currentThemeSetter: (theme: string) => void,
 }
 
-export default function Header(props : headerProps) {
-    function changeTheme(){
+export default function Header(props: headerProps) {
+    function changeTheme() {
         props.currentThemeSetter("lightTheme")
     }
+
     return (
         <div className={styles.Header}>
-            <NavLink to={'/'} >
+            <NavLink to={'/'}>
                 <FadeInText textToDisplay={'siffert[.io]'} mainFontSize={"5vh"}/>
                 <FadeInText textToDisplay={'a work in progress'} mainFontSize={"2vh"} leftToRightAnimation={false}/>
             </NavLink>
-            <button onClick={changeTheme}>
-                Theme
-            </button>
+            <div className={styles.buttonContainer}>
+                <button onClick={changeTheme}>
+                    Theme
+                </button>
+            </div>
         </div>
     );
 }
