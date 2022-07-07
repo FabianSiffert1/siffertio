beb88fba6d1d8b54eb9e87205dfee0648a397a3f
Fabian Siffert <siffert@sipgate.de>
2022-07-07 10:41:46 +0200

    :recycle: random stuff i guess. sometimes Im confused

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -5,22 +5,35 @@ import NavigationBar from "../NavigationBar/NavigationBar";
 import Header from "../Header/Header";
 
 const Home = () => {
+    const [currentTheme, setCurrentTheme] = useState<string>("darkTheme");
+
     const currentLocation = useLocation().pathname;
     let welcomeScreen = undefined;
     if (currentLocation === "/") {
         welcomeScreen = true;
     }
 
-    const [currentTheme, setCurrentTheme] = useState<string>("darkTheme");
+    let darkTheme = false;
+    let lightTheme = false;
+    let nextTheme = false;
+
+    switch (currentTheme){
+        case("darkTheme"):
+            darkTheme = true;
+            break;
+        case("lightTheme"):
+            lightTheme = true;
+            break;
+    }
 
     return (
         <div className={styles.Home}>
-            {currentTheme}
-            {welcomeScreen ? "" : [<Header currentThemeSetter = {setCurrentTheme}/>]}
-            <div className={welcomeScreen? styles.welcomeScreen : styles.contentContainer}>
-                <Outlet/>
-            </div>
-            <NavigationBar routerPosition={currentLocation}/>
+                {currentTheme}
+                {welcomeScreen ? "" : [<Header currentThemeSetter={setCurrentTheme}/>]}
+                <div className={welcomeScreen ? styles.welcomeScreen : styles.contentContainer}>
+                    <Outlet/>
+                </div>
+                <NavigationBar routerPosition={currentLocation}/>
         </div>
     );
 }
