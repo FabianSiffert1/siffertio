a05ec57c0e533c03de4d0931d772bdea526f97b7
Fabian Siffert <siffert@sipgate.de>
2022-07-15 08:29:52 +0200

    :recycle: refactor Contact-View into Contact-View and SideBarRouter-Component

--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -1,44 +1,11 @@
-import {NavLink, Outlet} from "react-router-dom";
 import styles from "./Contact.module.scss";
-import {getInvoices} from "../../tmpData/data";
-import language from "../../resources/language/language";
-import {useSelector} from "react-redux";
+import SideBarRouter from "../../components/SideBarRouter/SideBarRouter";
+
 
 export default function Contact() {
-    const currentTheme = useSelector((state: any) => state.theme.value)
-    let invoices = getInvoices();
     return (
         <div className={styles.Contact}>
-            <div
-                className={currentTheme === language.THEME_DARK_VAR ? styles.darkTheme : currentTheme === language.THEME_LIGHT_VAR ? styles.lightTheme : styles.colorfulTheme}>
-                <nav
-                    style={{
-                        borderRight: "solid 0.2vw",
-                        padding: "0.5vw",
-                        minWidth: "12vw",
-                    }}
-                >
-                    {invoices.map((invoice) => (
-                        <NavLink className={styles.navigationItem}
-                            style={({isActive}) => {
-                                return {
-                                    display: "block",
-                                    margin: "1vh 0",
-                                    border: isActive ? "solid" : "",
-                                    borderRadius: "30vw",
-                                    padding: "0.5vw",
-                                };
-                            }
-                            }
-                            to={`/contact/${invoice.number}`}
-                            key={invoice.number}
-                        >
-                            {invoice.name}
-                        </NavLink>
-                    ))}
-                </nav>
-                <Outlet/>
-            </div>
+            <SideBarRouter/>
         </div>
     );
 }
