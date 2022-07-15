55a608875e40d7133f4f4529140b8f1c8c5a61a2
Fabian Siffert <siffert@sipgate.de>
2022-07-15 08:54:54 +0200

    :recycle: WIP: Refactor Biography View to Biography View and FadeInText. Also rename old fadeInText to ColoredBarsAndFadeInText

---
+++ src/components/ColoredBarsAndFadeInText/FadeInText.tsx
@@ -0,0 +1,52 @@
+import styles from "./FadeInText.module.scss";
+import React from "react";
+
+export interface FadeInTextProps {
+    textToDisplay?: string,
+    mainBlockColor?: string,
+    mainBlockHeight?: string,
+    mainBlockWidth?: string,
+    mainFontSize?: string,
+    secondaryFontSize?: string,
+    leftToRightAnimation?: boolean,
+    children?: React.ReactNode,
+}
+
+export default function FadeInText(props: FadeInTextProps) {
+    return (
+        <div className={styles.FadeInTextContainer}>
+            <div className={styles.fadeInTitleContainer}>
+                <div className={styles.fadeInAnimationContainer}>
+                    <div className={styles.fadeinMainTitleContainer}>
+                        <div
+                            className={props.leftToRightAnimation ? styles.coloredBlockLeftToRight : styles.coloredBlockRightToLeft}
+                            style={{
+                                backgroundColor: props.mainBlockColor,
+                                height: props.mainBlockHeight,
+                                width: props.mainBlockWidth,
+                            }}
+                        ></div>
+                        {props.textToDisplay &&
+                            <div className={styles.fadeInMainTitleText} style={{
+                                fontSize: props.mainFontSize,
+                            }}>
+                                {props.textToDisplay}
+                            </div>
+                        }
+                    </div>
+                </div>
+                {props.children &&
+                    <div className={styles.fadeInSubtitleContainer} style={{
+                        fontSize: props.secondaryFontSize,
+                    }}>
+                        {props.children}
+                    </div>
+                }
+            </div>
+        </div>
+
+    );
+}
+FadeInText.defaultProps = {
+    leftToRightAnimation: true,
+}
