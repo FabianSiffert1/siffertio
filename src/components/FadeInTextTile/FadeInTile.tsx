1db49c94a10a66739c98c54b0652238d4e9634cc
Fabian Siffert <siffert@sipgate.de>
2022-07-15 22:11:25 +0200

    :recycle: finish refactor/split of biography into view and fadeInTile component

--- src/components/FadeInTextTile/FadeInTile.tsx
+++ src/components/FadeInTextTile/FadeInTile.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import styles from "./ThemeDropdownMenu.module.scss";
+import styles from "./FadeInTile.module.scss";
+
 import {useSelector} from "react-redux";
 import language from "../../resources/language/language";
 
-export interface FadeInTextProps {
+export interface FadeInTileProps {
+    tileTitle?: string;
     children: any;
 }
 
-export function FadeInText(props: FadeInTextProps) {
+export function FadeInTile(props: FadeInTileProps) {
     const currentTheme = useSelector((state: any) => state.theme.value)
 
     return (
-        <div className={styles.FadeInText}>
+        <div className={styles.FadeInTile}>
             <div
                 className={currentTheme === language.THEME_DARK_VAR ? styles.darkTheme : currentTheme === language.THEME_LIGHT_VAR ? styles.lightTheme : styles.colorfulTheme}>
-                <div className={styles.textContent}>
+                {props.tileTitle && <div className={styles.tileTitle}>{props.tileTitle}</div>}
+                <div className={styles.tileContent}>
                     {props.children}
                 </div>
             </div>
--- src/views/Biography/Biography.tsx
+++ src/views/Biography/Biography.tsx
@@ -1,39 +1,32 @@
 import styles from "./Biography.module.scss";
 import language from "../../resources/language/language";
 import {useSelector} from "react-redux";
+import {FadeInTile} from "../../components/FadeInTextTile/FadeInTile";
 
 export default function Biography() {
     const currentTheme = useSelector((state: any) => state.theme.value)
     return (
         <div className={styles.Biography}>
-            <div
-                className={currentTheme === language.THEME_DARK_VAR ? styles.darkTheme : currentTheme === language.THEME_LIGHT_VAR ? styles.lightTheme : styles.colorfulTheme}>
-                <div className={styles.textContainer}>
-                    <div className={styles.textTitle}>
-                        Biography
-                    </div>
-                    <div className={styles.textContent}>
-                        <p>
-                            {language.LOREM}
-                        </p>
-                        <p>
-                            {language.LOREM}
-                        </p>
-                        <p>
-                            {language.LOREM}
-                        </p>
-                        <p>
-                            {language.LOREM}
-                        </p>
-                        <p>
-                            {language.LOREM}
-                        </p>
-                        <p>
-                            {language.LOREM}
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <FadeInTile tileTitle={'Biography'}>
+                <p>
+                    {language.LOREM}
+                </p>
+                <p>
+                    {language.LOREM}
+                </p>
+                <p>
+                    {language.LOREM}
+                </p>
+                <p>
+                    {language.LOREM}
+                </p>
+                <p>
+                    {language.LOREM}
+                </p>
+                <p>
+                    {language.LOREM}
+                </p>
+            </FadeInTile>
         </div>
     );
 }
