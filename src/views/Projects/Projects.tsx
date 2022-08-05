e53c420e0575770618e69d99b646903da24620d0
Fabian Siffert <siffert@sipgate.de>
2022-08-05 18:19:13 +0200

    :lipstick: some svg stuff

--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -1,16 +1,16 @@
-import {useSelector} from "react-redux";
-
 import styles from "./Projects.module.scss";
 import language from "../../assets/language/language";
+import {TableSection} from "../../components/TableSection/TableSection";
 
 export default function Projects() {
-    const currentTheme = useSelector((state: any) => state.theme.value)
+
     return (
         <div className={styles.Projects}>
-            <div
-                className={currentTheme === language.THEME_DARK_VAR ? styles.darkTheme : currentTheme === language.THEME_LIGHT_VAR ? styles.lightTheme : styles.colorfulTheme}>
-                {language.PROJECTS_TITLE}
-            </div>
+            <TableSection tableTitle={"Things I've done (as a programmer)"}>
+                <div>{language.HEADER_TITLE}</div>
+
+                <div>{language.WELCOME_SUBTITLE}</div>
+            </TableSection>
         </div>
     );
 }
