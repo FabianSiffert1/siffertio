c6b17f8e3cf138d247b84ab6cefeb02e30ad2db5
Fabian Siffert <siffert@sipgate.de>
2022-10-29 16:39:47 +0200

    ♻️💄implement boxShadows everywhere, refactor NavBar

--- src/components/AnimatedTextRouter/AnimatedTextRouter.tsx
+++ src/components/AnimatedTextRouter/AnimatedTextRouter.tsx
@@ -1,37 +1,47 @@
-import React from "react"
-import styles from "./AnimatedTextRouter.module.scss"
+import React from "react";
+import styles from "./AnimatedTextRouter.module.scss";
 
-import {NavLink, useLocation} from "react-router-dom"
-import language from "../../assets/language/language"
-import {useSelector} from "react-redux"
-import {RootState} from "../../utils/reduxStore/reduxStore"
+import { NavLink, useLocation } from "react-router-dom";
+import language from "../../assets/language/language";
+import { useSelector } from "react-redux";
+import { RootState } from "../../utils/reduxStore/reduxStore";
 
 export interface AnimatedTextRouterProps {
-    routesArray: { route: string; routeKey: string; }[];
-    style?: string;
-    currentTheme?: string;
+  routesArray: { route: string; routeKey: string }[];
+  style?: string;
+  currentTheme?: string;
 }
-
 
 function AnimatedTextRouter(props: AnimatedTextRouterProps) {
-	const currentTheme = useSelector((state: RootState) => state.theme.value)
-	const currentLocation = useLocation().pathname
-	return (<>
-		{props.routesArray.map((element) => <div className={styles.AnimatedTextRouter} key={element.routeKey}>
-			<NavLink
-				to={element.route}
-				key={element.routeKey}
-			>
-				<div
-					className={currentLocation === element.route ? currentTheme === language.THEME_DARK_VAR ? styles.activeLinkDarkTheme : currentTheme === language.THEME_LIGHT_VAR ? styles.activeLinkLightTheme : styles.activeLinkColorfulTheme : styles.inActiveLink}>
-					{element.routeKey}
-				</div>
-			</NavLink>
-		</div>
-		)}
-	</>
-	)
+  const currentTheme = useSelector((state: RootState) => state.theme.value);
+  const currentLocation = useLocation().pathname;
+  return (
+    <>
+      {props.routesArray.map((element) => (
+        <div className={styles.AnimatedTextRouter} key={element.routeKey}>
+          <NavLink to={element.route} key={element.routeKey}>
+            <div
+              className={
+                currentLocation === element.route
+                  ? currentTheme === language.THEME_DARK_VAR
+                    ? styles.activeLinkDarkTheme
+                    : currentTheme === language.THEME_LIGHT_VAR
+                    ? styles.activeLinkLightTheme
+                    : styles.activeLinkColorfulTheme
+                  : currentTheme === language.THEME_DARK_VAR
+                  	? styles.inactiveLinkDarkTheme
+                  : currentTheme === language.THEME_LIGHT_VAR
+                  ? styles.inactiveLinkLightTheme
+                  : styles.inactiveLinkColorfulTheme
+              }
+            >
+              {element.routeKey}
+            </div>
+          </NavLink>
+        </div>
+      ))}
+    </>
+  );
 }
 
-
-export default AnimatedTextRouter
+export default AnimatedTextRouter;
--- src/components/DropdownMenuTheme/DropdownMenuTheme.tsx
+++ src/components/DropdownMenuTheme/DropdownMenuTheme.tsx
@@ -1,48 +1,72 @@
-import React, {useState} from "react"
-import styles from "./DropdownMenuTheme.module.scss"
-import {useDispatch, useSelector} from "react-redux"
-import {changeToTheme} from "../../utils/reduxStore/theme/themeSlice"
-import language from "../../assets/language/language"
-import {RootState} from "../../utils/reduxStore/reduxStore"
+import React, { useState } from "react";
+import styles from "./DropdownMenuTheme.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { changeToTheme } from "../../utils/reduxStore/theme/themeSlice";
+import language from "../../assets/language/language";
+import { RootState } from "../../utils/reduxStore/reduxStore";
 
 export interface ThemeDropdownMenuProps {
-    menuTitle: string;
-    menuElements: { elementName: string; elementFunction: string; }[];
+  menuTitle: string;
+  menuElements: { elementName: string; elementFunction: string }[];
 }
 
 export function DropdownMenuTheme(props: ThemeDropdownMenuProps) {
-	const [display, setDisplay] = useState("none")
-	const currentTheme = useSelector((state: RootState) => state.theme.value)
-	const dispatch = useDispatch()
+  const [display, setDisplay] = useState("none");
+  const currentTheme = useSelector((state: RootState) => state.theme.value);
+  const dispatch = useDispatch();
 
-	function handleClick() {
-		if (display === "none") {
-			setDisplay("block")
-		} else setDisplay("none")
-	}
+  function handleClick() {
+    if (display === "none") {
+      setDisplay("block");
+    } else setDisplay("none");
+  }
 
-	function changeTheme(themeToDisplay: string) {
-		dispatch(changeToTheme(themeToDisplay))
-		setDisplay("none")
-	}
+  function changeTheme(themeToDisplay: string) {
+    dispatch(changeToTheme(themeToDisplay));
+    setDisplay("none");
+  }
 
-	return (
-		<div className={styles.DropdownMenu}>
-			<div onClick={handleClick}
-				className={currentTheme === language.THEME_DARK_VAR ? styles.dropdownButtonDarkTheme : currentTheme === language.THEME_LIGHT_VAR ? styles.dropdownButtonLightTheme : styles.dropdownButtonColorfulTheme}>
-				{props.menuTitle}
-			</div>
-			<div className={styles.dropdownElements} style={{display: display}}>
-				{props.menuElements.map((element) =>
-					<div
-						className={currentTheme === language.THEME_DARK_VAR ? styles.elementDarkTheme : currentTheme === language.THEME_LIGHT_VAR ? styles.elementLightTheme : styles.elementColorfulTheme}
-						key={element.elementName}
-						onClick={() => changeTheme(element.elementFunction)}>{element.elementName}</div>
-				)
-				}
-
-			</div>
-
-		</div>
-	)
+  return (
+    <div className={styles.DropdownMenu}>
+      <div
+        onClick={handleClick}
+        className={
+          currentTheme === language.THEME_DARK_VAR
+            ? styles.dropdownButtonDarkTheme
+            : currentTheme === language.THEME_LIGHT_VAR
+            ? styles.dropdownButtonLightTheme
+            : styles.dropdownButtonColorfulTheme
+        }
+      >
+        {props.menuTitle}
+      </div>
+      <div className={styles.dropdownElements} style={{ display: display }}>
+        {props.menuElements.map((element) => (
+          <div
+            className={
+              currentTheme === language.THEME_DARK_VAR
+                ? styles.elementDarkTheme
+                : currentTheme === language.THEME_LIGHT_VAR
+                ? styles.elementLightTheme
+                : styles.elementColorfulTheme
+            }
+            key={element.elementName}
+            style={{
+              border:
+                currentTheme === language.THEME_DARK_VAR &&
+                element.elementName == language.THEME_DARK
+                  ? "0.2vw solid #ff6f69"
+                  : currentTheme === language.THEME_LIGHT_VAR &&
+                    element.elementName == language.THEME_LIGHT
+                  ? "solid #ff6f69"
+                  : "",
+            }}
+            onClick={() => changeTheme(element.elementFunction)}
+          >
+            {element.elementName}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
