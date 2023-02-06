19bcd7b98e8cd5a26f6cfb34005c2358a2137f7e
Fabian Siffert <siffert@sipgate.de>
2023-02-06 16:14:52 +0100

    💄 minor changes to experienceIndicatorToggle Positioning

--- src/components/SkillBanner/SkillBanner.tsx
+++ src/components/SkillBanner/SkillBanner.tsx
@@ -4,13 +4,22 @@ import language from "../../assets/language/language";
 import { useSelector } from "react-redux";
 import { RootState } from "../../utils/reduxStore/reduxStore";
 
+enum experienceLevel {
+    beginner,
+    moderate,
+    good,
+    expert
+}
+
 export interface SkillBannerProps {
   text: string;
   image: string;
+  experienceLevel?: experienceLevel;
 }
 
 export default function SkillBanner(props: SkillBannerProps) {
   const currentTheme = useSelector((state: RootState) => state.theme.value);
+  let showExperience = useSelector((state: RootState) => state.experienceToggle.value);
   return (
     <div className={styles.SkillBanner}>
       <div
